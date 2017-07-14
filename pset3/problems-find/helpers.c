b83ca0c1ee1ed656e6fc48666e24e8f62666d480
RebeccaChen1 <rebeccachentao@gmail.com>
2017-07-14 17:57:59 -0400

    Add files via upload

---
+++ pset3/problems-find/helpers.c
@@ -0,0 +1,64 @@
+/**
+ * helpers.c
+ *
+ * Helper functions for Problem Set 3.
+ */
+       
+#include <cs50.h>
+
+#include "helpers.h"
+
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // TODO: implement a searching algorithm
+    if (value < 0)
+    {
+        return false;
+    }
+    int low = 0;
+    int high = n - 1;
+    while(n > 0)
+    {
+        int mid = (high - low) / 2 + low;
+        
+        if (values[mid] < value)
+        {
+            low = mid + 1;
+        }
+        else if(values[mid] > value)
+        {
+            high = mid - 1;
+        }
+        else if(values[mid] == value)
+        {
+            return true;
+        }
+        n = high - low + 1;
+    }
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement an O(n^2) sorting algorithm
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if (values[j] > values[j + 1])
+            {
+                int temp = values[j];
+                values[j] = values[j + 1];
+                values[j + 1] = temp;
+            }
+       }
+    }
+    return;
+}
